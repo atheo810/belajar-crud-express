8a54e49227b09e759641d935fb3c1369cd8cd436
atheo810 <atheos810@gmail.com>
2023-03-23 15:16:54 +0800

    menambahkan updateUser dan deleteUser pada controller/UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -36,3 +36,29 @@ export const createUser = async (req, res) => {
     console.log(error.message);
   }
 };
+
+export const updateUser = async (req, res) => {
+  try {
+    await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "User telah di update" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: "User telah dihapus" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -6,6 +6,8 @@ import {
   getUser,
   getUserById,
   createUser,
+  updateUser,
+  deleteUser,
 } from "../controllers/UserController.js";
 
 // membuat contraint router dengan menggunakan fitur Router() pada express
@@ -15,6 +17,8 @@ const router = express.Router();
 router.get("/users", getUser);
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 // export router
 export default router;
