5d2d8b50490f92216204c71e2c3b7be5dd1aaf79
atheo810 <atheos810@gmail.com>
2023-03-24 10:11:37 +0800

    melakukan setting konfigurasi terhadap route
    setting routes nantinya akan menjalankan function yang ada pada controller/ProductController.js

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -6,7 +6,14 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/ProductController";
+import { getUserById } from "../controllers/UserController";
 
 const router = express.Router();
+
+router.get("/product", getProducts);
+router.get("/product/:id", getUserById);
+router.post("/product", createProduct);
+router.patch("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
 
 export default router;
